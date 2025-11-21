e2ada69232743b8b0993ffc86791e63bbad2c865
kittykaraaira <kittykaraa@gmail.com>
2025-11-21 11:57:45 +0700

    Implement Landing Page & AdminLTE Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[LandingController::class,'index'])->name('landing');
+
+Route::prefix('admin')->name('admin.')->group(function(){
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
 });
