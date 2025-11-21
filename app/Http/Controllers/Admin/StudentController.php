0e58042e4985cc701b6c7934b54e4481f975566a
kittykaraaira <kittykaraa@gmail.com>
2025-11-21 22:45:53 +0700

    Add Feature Create Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -16,15 +16,23 @@ class StudentController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.student.create');
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nis'=>'required|unique:students',
+            'nama_lengkap'=>'required',
+            'jenis_kelamin'=>'required',
+            'nisn'=>'required|unique:students'
+        ]);
+        Student::create($request->all());
+        return redirect()->route('admin.students.index')->with('succes','Data berhasil disimpan');
     }
 
     /**
